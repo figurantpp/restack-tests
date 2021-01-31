94bcd043c873bfe5d9cdd60d0be529feb7f80fbc
figurantpp <renan.r.marcelino2@protonmail.com>
2021-01-31 10:52:25 -0300

    Improved naming

--- test.c
+++ test.c
@@ -19,7 +19,9 @@ struct TestObject
 };
 
 
-static long test_object_value = 0;
+static unsigned long test_object_value = 0;
+
+#define TEST_INITIAL_VALUE 0xbeef
 
 void increase_test_object_value_by(int count)
 {
@@ -80,12 +82,12 @@ void test_stack(CuTest *test)
     struct TestObject *objects[TEST_SIZE];
 
     RestackResource resources[TEST_SIZE];
-    ResourceStack stack[1];
+    Restack stack[1];
     size_t i;
 
     restack_init_macro(stack, resources);
 
-    test_object_value = 0;
+    test_object_value = TEST_INITIAL_VALUE;
     test_object_delete_count = 0;
 
     for (i = 0; i < TEST_SIZE; i++)
@@ -96,14 +98,14 @@ void test_stack(CuTest *test)
 
     restack_delete(stack);
 
-    CuAssertIntEquals(test, test_object_value, 0);
+    CuAssertIntEquals(test, test_object_value, TEST_INITIAL_VALUE);
     CuAssertIntEquals(test, test_object_delete_count, TEST_SIZE);
 }
 
 
 void test_isolated_push(CuTest *test)
 {
-    ResourceStack stack[1];
+    Restack stack[1];
 
     RestackResource resources[3];
 
@@ -111,28 +113,28 @@ void test_isolated_push(CuTest *test)
     restack_init(stack, resources, 3);
 
 
-    test_object_value = 0;
+    test_object_value = TEST_INITIAL_VALUE;
 
     increase_test_object_value();
     increase_test_object_value();
     increase_test_object_value();
 
-    restack_push_isolated(stack, decrease_test_object_value);
-    restack_push_isolated(stack, decrease_test_object_value);
-    restack_push_isolated(stack, decrease_test_object_value);
+    restack_push_call(stack, decrease_test_object_value);
+    restack_push_call(stack, decrease_test_object_value);
+    restack_push_call(stack, decrease_test_object_value);
 
     restack_delete(stack);
 
-    CuAssertIntEquals(test, test_object_value, 0);
+    CuAssertIntEquals(test, test_object_value, TEST_INITIAL_VALUE);
 }
 
 void test_null_push(CuTest *test)
 {
     RestackResource resources[3];
-    ResourceStack stack[1];
+    Restack stack[1];
     restack_init(stack, resources, 3);
 
-    test_object_value = 0;
+    test_object_value = TEST_INITIAL_VALUE;
 
     restack_push(stack, NULL, test_object_delete);
     restack_push(stack, NULL, test_object_delete);
@@ -140,7 +142,46 @@ void test_null_push(CuTest *test)
 
     restack_delete(stack);
 
-    CuAssertIntEquals(test, test_object_value, 0);
+    CuAssertIntEquals(test, test_object_value, TEST_INITIAL_VALUE);
+}
+
+
+void test_overflow_handler(void)
+{
+    test_object_value = 0xfeedbeef;
+}
+
+void test_overflow(CuTest *test)
+{
+    struct TestObject *objects[2];
+
+    RestackResource resources[1];
+    Restack stack;
+
+    restack_init_macro(&stack, resources);
+
+    test_object_value = TEST_INITIAL_VALUE;
+
+    restack_set_overflow_handler(test_overflow_handler);
+
+    objects[0] = test_object_init();
+    objects[1] = test_object_init();
+
+    restack_push(&stack, objects[0], test_object_delete);
+
+    /*
+     * On overflow, the overflow object should not be deleted, no buffer overflow shall occur.
+     * An optional error message may be emitted.
+     */
+    restack_push(&stack, objects[1], test_object_delete);
+
+    restack_delete(&stack);
+
+    CuAssert(test, "Test value shall be unmatched", test_object_value != TEST_INITIAL_VALUE);
+
+    /* Restack shouldn't have the last object so we have to do it ourselves */
+
+    test_object_delete(objects[1]);
 }
 
 int main(void)
@@ -152,6 +193,7 @@ int main(void)
     SUITE_ADD_TEST(spec, test_stack);
     SUITE_ADD_TEST(spec, test_isolated_push);
     SUITE_ADD_TEST(spec, test_null_push);
+    SUITE_ADD_TEST(spec, test_overflow);
 
     output = CuStringNew();
     suite = CuSuiteNew();
